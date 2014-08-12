57801767917c272f7d989f3e59aafc2691a8ac38
Brock Allen <brockallen@gmail.com>
2014-08-12 18:51:14 -0400

    mark props as virtual for EF

--- source/Host/MembershipRebootIdentityManagerFactory.cs
+++ source/Host/MembershipRebootIdentityManagerFactory.cs
@@ -14,10 +14,10 @@ namespace Thinktecture.IdentityManager.Host
     public class CustomUser : RelationalUserAccount
     {
         [Display(Name="First Name")]
-        public string FirstName { get; set; }
+        public virtual string FirstName { get; set; }
         [Display(Name = "Last Name")]
-        public string LastName { get; set; }
-        public int? Age { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual int? Age { get; set; }
     }
 
     public class CustomDatabase : MembershipRebootDbContext<CustomUser>
